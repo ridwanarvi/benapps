824502e3c380e496c747cc3438b0e7f93bca8dd7
Vincenda Suharyono <mail@vesperia.id>
2017-03-17 13:16:42 +0700

    Initial Commit

---
+++ BEN AppsTests/BEN_AppsTests.swift
@@ -0,0 +1,36 @@
+//
+//  BEN_AppsTests.swift
+//  BEN AppsTests
+//
+//  Created by Vesperia on 3/17/17.
+//  Copyright © 2017 Vesperia. All rights reserved.
+//
+
+import XCTest
+@testable import BEN_Apps
+
+class BEN_AppsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
